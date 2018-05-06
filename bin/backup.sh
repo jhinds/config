3e6b0bcc6451f44f845c4725d96c9e04c4b2c08e
Jonathan Hinds <jhinds2010@gmail.com>
2018-05-06 13:49:08 -0400

    Backuping up some configs

--- bin/backup.sh
+++ bin/backup.sh
@@ -5,12 +5,16 @@ set -o pipefail
 
 echo -n 'Backing Up'
 
-git checkout -b backup/$(date "+%F")
-brew bundle list --all
+# git checkout -b backup/$(date "+%F")
+brew bundle dump --force
+cp ~/.gitconfig ~/config/.gitconfig
+cp ~/.gitignore ~/config/.gitignore
+cp -r ~/Development/.git-hooks/* ~/config/.git-hooks/*
+cp ~/.vimrc ~/config/.vimrc
+apm ls --bare  --installed  > ~/config/atom_packages
+cp ~/.config/fish/config.fish ~/config/config.fish
 
-
-
-git add .
-git commit -m 'Latest Backup'
-git push --set-upstream origin backup/$(date "+%F")
-git push
+# git add .
+# git commit -m 'Latest Backup'
+# git push --set-upstream origin backup/$(date "+%F")
+# git push
---
+++ bin/bootstrap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+mkdir -p ~/Development
+# # install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# # copy over brewfile & install
+brew bundle
+
+# # force link imagemagick
+# brew link imagemagick@6 --force
+#
+# # make .logs directory
+# mkdir ~/.logs
+#
+# # copy over bash_profile
+# cp .bash_profile ~/.bash_profile
+# source ~/.bash_profile
+cp config.fish ~/.config/fish/config.fish
+
+# # install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#
+# # copy over vimrc
+cp .vimrc ~/.vimrc
+
+# # copy over gitignore
+cp .gitignore ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
+
+# make folder for git hooks
+mkdir -p ~/Development/.git-hooks
+cp -r .git-hooks/ ~/Development/.git-hooks/
+git config --global core.hooksPath ~/Development/.git-hooks/
+
+# install atom packages
+apm install --packages-file ~/config/atom_packages
+
+# # install python requirements
+# pip3 install -r requirements.txt
