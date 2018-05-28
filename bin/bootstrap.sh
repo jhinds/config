01ffbf38927e37b279c5fe79d9a2c1e1e354317d
Jonathan Hinds <jhinds2010@gmail.com>
2018-05-28 17:17:41 -0400

    New bootstrap file

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,32 +3,49 @@
 set -e
 set -o pipefail
 
-echo -n "Bootstrapping\n"
+printf "Bootstrapping\n"
 
 # make development directories
 mkdir -p ~/Development
 mkdir -p ~/Development/.git-hooks
 
-# # install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install homebrew
+if ! [ -x "$(which brew)" ]; then
+  echo 'Homebrew is not installed. Going to install Homebrew' >&2
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-# # copy over brewfile & install
+# copy over brewfile & install
+brew update
 brew bundle
 
-cp config.fish ~/.config/fish/config.fish
+cp ./config.fish ~/.config/fish/config.fish
 
-# # install Vundle
-! git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-#
+# Get default shell
+DEFAULT_SHELL=$(finger $(whoami) | grep -o "Shell:.*" | awk -F ": " '{print $NF}')
+
+if [ "$DEFAULT_SHELL" != "$(which fish)" ]; then
+  echo 'Fish is not the default shell. Making Fish the default shell' >&2
+  chsh -s $(which fish) $(whoami)
+fi
+
+
+# install Vundle
+if [ ! -d "~/.vim/bundle/Vundle.vim" ]; then
+  echo 'Vundle is not installed. Going to install Vundle' >&2
+  ! git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 # # copy over vimrc
 cp .vimrc ~/.vimrc
 
 # # copy over gitignore
 cp .gitignore ~/.gitignore
+cp .gitconfig ~/.gitconfig
 git config --global core.excludesfile ~/.gitignore
 
 # make folder for git hooks
-cp -r .git-hooks/ ~/Development/.git-hooks/
+cp -r ./.git-hooks ~/Development/.git-hooks/
 git config --global core.hooksPath ~/Development/.git-hooks/
 
 # install atom packages
